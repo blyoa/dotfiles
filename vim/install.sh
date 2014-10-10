32af36728a9ecc378e41fb446911b4d94e7f3695
blyoa <yblue110+blyoa@gmail.com>
2014-10-10 12:06:18 +0900

    Add installer of vim

---
+++ vim/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+cd ${0%/*}
+source ../script/functions/*.sh
+
+# install script below 
+symlink $(pwd)"/.vimrc" ~/.vimrc
+
+# install NeoBundle 
+if [ ! -e ~/.vim/bundle/neobundle.vim ]; then
+    git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+fi
