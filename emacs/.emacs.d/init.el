4d25280485ac5f734f070ee9094a77263c2c0fe4
blyoa <blyoa@users.noreply.github.com>
2014-10-10 12:06:48 +0900

    Add installer of emacs

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,5 @@
+(add-to-list 'load-path user-emacs-directory)
+(add-to-list 'load-path (expand-file-name "config" user-emacs-directory))
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+(require 'init-yatex-mode)
