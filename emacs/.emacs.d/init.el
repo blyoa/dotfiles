912e84336c460b82cf5f7c6199219212de4b4c23
blyoa <blyoa@users.noreply.github.com>
2015-04-01 21:03:58 +0900

    Add setting of el-get

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -4,4 +4,16 @@
 (add-to-list 'load-path (expand-file-name "config" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+    (url-retrieve-synchronously
+      "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+(el-get 'sync)
+
 (require 'init-yatex-mode)
