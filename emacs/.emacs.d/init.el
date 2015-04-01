a30f7ed7b23185582334b7da635d56c3c57b2766
blyoa <blyoa@users.noreply.github.com>
2015-04-01 20:59:09 +0900

    Add setting of user-emacs-directory

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,3 +1,5 @@
+(when load-file-name
+  (setq user-emacs-directory (file-name-directory load-file-name)))
 (add-to-list 'load-path user-emacs-directory)
 (add-to-list 'load-path (expand-file-name "config" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
