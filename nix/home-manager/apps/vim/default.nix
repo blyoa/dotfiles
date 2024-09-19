46d2e06359728196b91268048fd8fbd791a95c01
blyoa <blyoa@users.noreply.github.com>
2024-09-19 11:48:09 +0900

    feat(nix): install vim-plug with Nix

--- nix/home-manager/apps/vim/default.nix
+++ nix/home-manager/apps/vim/default.nix
@@ -1,6 +1,20 @@
-{ config, params, ... }:
+{
+  pkgs,
+  config,
+  params,
+  ...
+}:
 {
   home.file.".vimrc" = {
     source = config.lib.file.mkOutOfStoreSymlink "${params.appsAbsPath}/vim/.vimrc";
+  };
+
+  home.file.".vim/bundle/vim-plug/autoload/" = {
+    source = pkgs.fetchFromGitHub {
+      owner = "junegunn";
+      repo = "vim-plug";
+      rev = "0.14.0";
+      hash = "sha256-d8LZYiJzAOtWGIXUJ7788SnJj44nhdZB0mT5QW3itAY=";
+    };
   };
 }
