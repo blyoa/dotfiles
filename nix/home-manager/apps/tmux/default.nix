90c6ebb2663adfb2ea9d99a3a322e6137ebfb03e
blyoa <blyoa@users.noreply.github.com>
2024-09-19 12:10:10 +0900

    feat(nix): install tpm with Nix

--- nix/home-manager/apps/tmux/default.nix
+++ nix/home-manager/apps/tmux/default.nix
@@ -12,4 +12,13 @@
   xdg.configFile."tmux/tmux.conf" = {
     source = config.lib.file.mkOutOfStoreSymlink "${params.appsAbsPath}/tmux/.tmux.conf";
   };
+
+  home.file.".tmux/plugins/tpm/" = {
+    source = pkgs.fetchFromGitHub {
+      owner = "tmux-plugins";
+      repo = "tpm";
+      rev = "99469c4a9b1ccf77fade25842dc7bafbc8ce9946";
+      hash = "sha256-hW8mfwB8F9ZkTQ72WQp/1fy8KL1IIYMZBtZYIwZdMQc=";
+    };
+  };
 }
