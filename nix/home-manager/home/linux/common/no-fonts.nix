5a3752ca7e276741d44b16f202aaaad307414fc1
blyoa <blyoa@users.noreply.github.com>
2025-01-01 15:03:01 +0900

    fix(nix): use imports instead of import to easily merge configuration

--- nix/home-manager/home/linux/common/no-fonts.nix
+++ nix/home-manager/home/linux/common/no-fonts.nix
@@ -4,15 +4,15 @@
   params,
   ...
 }:
-let
-  common = import ../../common { inherit pkgs inputs; };
-in
 {
+  imports = [
+    ../../common
+    ../../../fonts
+  ];
+
   home.username = "${params.userName}";
   home.homeDirectory = "/home/${params.userName}";
   home.stateVersion = "24.11";
 
-  programs = common.programs;
-
-  home.packages = common.home.packages ++ params.additionalPackages;
+  home.packages = params.additionalPackages;
 }
