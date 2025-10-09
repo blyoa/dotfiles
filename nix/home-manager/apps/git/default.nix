fe765626e8336ac0fc8b546e7bb4cc82d7727bdc
blyoa <blyoa@users.noreply.github.com>
2025-10-09 09:19:19 +0900

    feat(git): add an alias to pick a branch to switch using fzf

--- nix/home-manager/apps/git/default.nix
+++ nix/home-manager/apps/git/default.nix
@@ -1,4 +1,118 @@
-{ config, params, ... }:
+{
+  config,
+  params,
+  pkgs,
+  ...
+}:
+let
+  fzf-git-switch = pkgs.writeShellApplication {
+    name = "fzf-git-switch";
+    runtimeInputs = with pkgs; [
+      coreutils
+      fzf
+      gawk
+      gnugrep
+      gnused
+    ];
+
+    text = ''
+      branch_exists() {
+        local ref="$1"
+        git show-ref --quiet --verify "$ref"
+      }
+
+      is_remote_branch() {
+        local branch="$1"
+        branch_exists "refs/remotes/$branch"
+      }
+
+      parse_remote_branch() {
+        local branch="$1"
+        local remotes
+        mapfile -t remotes < <(git remote | awk '{print length, $0}' | sort -rn | cut -d' ' -f2-)
+
+        for remote in "''${remotes[@]}"; do
+          local prefix="''${remote}/"
+          if [[ "$branch" == "$prefix"* ]]; then
+            local candidate="''${branch#"$prefix"}"
+            if branch_exists "refs/remotes/$remote/$candidate"; then
+              echo "$candidate"
+              return 0
+            fi
+          fi
+        done
+
+        echo "$branch"
+      }
+
+      switch_to_branch() {
+        local target="$1"
+        shift
+
+        if is_remote_branch "$target"; then
+          echo "Switching to ''${target} in DETACHED mode"
+          git switch --detach "$target" "$@"
+        else
+          git switch "$target" "$@"
+        fi
+      }
+
+      select_branch_with_fzf() {
+        local excluded_branches
+        mapfile -t excluded_branches < <(
+          git worktree list --porcelain \
+            | grep "^branch " \
+            | sed "s/^branch //" || true
+        )
+
+        local selected_branch
+        selected_branch=$(
+          git for-each-ref --format="%(refname)" refs/heads/ refs/remotes/ \
+            | grep -v -E "^refs/remotes/.*/HEAD$" \
+            | grep -v -F -x -f <(printf "%s\n" "''${excluded_branches[@]}") \
+            | sed -E "s#^refs/(heads|remotes)/##" \
+            | fzf --reverse --height 20% --exit-0 \
+                --preview "git log --oneline --graph --color=always --decorate {1}" \
+                --preview-window=right:50%
+        )
+
+        echo "$selected_branch"
+      }
+
+      switch_with_fzf() {
+        local selected_branch
+        selected_branch=$(select_branch_with_fzf)
+
+        if [ -z "''${selected_branch:-}" ]; then
+          exit 0
+        fi
+
+        local branch_name
+        branch_name=$(parse_remote_branch "$selected_branch")
+
+        if is_remote_branch "$selected_branch"; then
+          echo "Switching to ''${selected_branch} in DETACHED mode"
+          git switch --detach "$selected_branch" "$@"
+        else
+          git switch "$branch_name" "$@"
+        fi
+      }
+
+      main() {
+        if [ $# -gt 0 ]; then
+          switch_to_branch "$@"
+        else
+          switch_with_fzf "$@"
+        fi
+      }
+
+      main "$@"
+    '';
+  };
+      fi
+    '';
+  };
+in
 {
   programs.git = {
     enable = true;
@@ -55,6 +169,10 @@
           "~/Dropbox/sync/github"
           "~/develop/repos"
         ];
+      };
+
+      alias = {
+        "sw" = "!${fzf-git-switch}/bin/fzf-git-switch";
       };
     };
   };
